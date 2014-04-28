7212685b22c60ecee5106dd6fd5da89564216f54
benjamin-jones <serial.infection@gmail.com>
2014-04-27 23:39:07 -0400

    Updated named pipe and computation code

--- Simulation/Model.py
+++ Simulation/Model.py
@@ -1,6 +1,49 @@
 #!/usr/bin/python
+import csv
+import math
+import random
+import time
+import os
+from debug import Debug
 from optparse import OptionParser
 
+#Debugging interface
+d = Debug()
+
+class Model:
+	elements = []
+	def __init__(self,reader):
+		for (resistance,capacitance) in reader:
+			d.dprint("Model:Model()","Read R =" + resistance + " Ohm C = " + capacitance + " F") 
+			self.elements.append( (resistance,capacitance) )
+	def getElements(self):
+		return self.elements
+
+	def getElementImpedance(self,element,frequency):
+		(r,c) = element
+		z = complex(float(r),1.0 / ( float(c)*(frequency / (2*math.pi))))
+		d.dprint("Model:Model:getElementImpedance()","Calculated " + str(z))	
+		return z
+
+def generateData(fifo,model):
+	samples = []
+	totalImpedance = 0.0
+	current = 0.0
+	for (r,c) in model.getElements():
+		totalImpedance = totalImpedance + float(r)
+	totalImpedance = totalImpedance / 2
+	while True:
+		t = time.time()
+		while time.time() - t < 0.00025:
+			continue
+		sample = random.gauss(0.0,15.0)
+		current = sample / totalImpedance
+		samples.append(current)
+		for (r,c) in model.getElements():
+			sample = (current)*float(r)
+			samples.append(sample)
+		for sample in samples:
+			fifo.write(str(sample) + ":")
 def begin():
 	parser = OptionParser()
 	parser.add_option("-f", "--file", dest="filename", help="Use model file", metavar="FILE")
@@ -8,9 +51,35 @@ def begin():
 
 	(options, args) = parser.parse_args()
 
+	# Get command line options
 	if not options.filename:
 		print "Please supply a model file with -f"
-		exit(0) 
+		exit(0)
+	if options.verbose:
+		d.setDebug()
+		
+	
+	d.dprint("Model:begin()","Opening model file")
+	try:
+		# Open Model file and build Model object
+		csvfile = open(options.filename,"rb")
+		reader = csv.reader(csvfile,delimiter=',',quotechar='#')
+		model = Model(reader);
+		csvfile.close()	
+
+		# Create named pipe
+		os.mkfifo("npipe")
+		fifo = open("npipe",'w')
+		generateData(fifo,model)
+		fifo.close()
+		os.remove("npipe")		
+	except Exception as e:
+		print "Failure: ",
+		print e
+		os.remove("npipe") 	
+		exit(0)
+	return
+
 
 if __name__=="__main__":
 	begin()
--- Simulation/Processor.py
+++ Simulation/Processor.py
@@ -1,11 +1,67 @@
 #!/usr/bin/python
 import os
+import math
+class Measurement:
+	input = []
+	chan1 = []
+	chan2 = []
+	chan3 = []
+	chan4 = []
+	
+def print_stats(m):
+	total = 0.0
+	for i in m.input:
+		total = total + i*i
+	total = total / 1024.0
+	print 10*math.log(total)
+def estimate_tf(m):
+	print_stats(m)
+	del(m)
+def get_data(fifo):
+	temp = ""
+	sample = 0.0	
+	while True:
+		rbyte = fifo.read(1)
+		if rbyte != ":":
+			temp = temp + rbyte
+			continue
+		else:
+			sample = float(temp)
+			break
+	return sample
+
+class AddData: 
+	def __init__(self):
+		self.ctr = 0
+	def __call__(self,m,sample):
+		if self.ctr == 0:
+			m.input.append(sample)
+		elif self.ctr == 1:
+			m.chan1.append(sample)
+		elif self.ctr == 2:
+			m.chan2.append(sample)
+		elif self.ctr == 3:
+			m.chan3.append(sample)
+		elif self.ctr == 4:
+			m.chan4.append(sample)
+			self.ctr = 0
+			return
+		self.ctr = self.ctr + 1
+
 def begin():
+	m = Measurement()
+	sample_count = 0
 	try:
 		fifo = open("npipe","r")
+		add_data = AddData()	
 		while True:
-			sample = fifo.read(4)
-			print sample
+			if sample_count == 1024:
+				estimate_tf(m)
+				sample_count = 0
+				m = Measurement()
+			sample = get_data(fifo)
+			add_data(m,sample)	
+			sample_count = sample_count + 1
 	except Exception as e:
 		print "Failure: ",
 		print e

19fe826a79684b1ed067c328aa80dcff74805b6d
xooxies <xooxies@darmok.(none)>
2014-05-29 20:28:30 -0400

    Added scipy processing code

--- Simulation/Processor.py
+++ Simulation/Processor.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import os
 import math
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Measurement:
 	input = []
 	chan1 = []
@@ -13,9 +17,25 @@ def print_stats(m):
 	for i in m.input:
 		total = total + i*i
 	total = total / 1024.0
-	print 10*math.log(total)
-def estimate_tf(m):
+	print "Average input power: " + str(10*math.log(total)) + " dB"
+
+def print_fft(m, h1):
+	N = 1024
+	T = 1.0 / 4000.0
+	xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+	I = sp.fft(m.input)
+	C1 = sp.fft(m.chan1)
+	C2 = sp.fft(m.chan2)
+	C3  = sp.fft(m.chan3)
+	C4 = sp.fft(m.chan4)
+
+	h1.set_xdata(xf)
+	h1.set_ydata(np.abs(I[0:N/2]))
+	plt.draw()
+
+def estimate_tf(m,h1):
 	print_stats(m)
+	print_fft(m,h1)
 	del(m)
 def get_data(fifo):
 	temp = ""
@@ -54,9 +74,11 @@ def begin():
 	try:
 		fifo = open("npipe","r")
 		add_data = AddData()	
+		h1, = plt.plot([],[])
+		plt.show(block=False)
 		while True:
 			if sample_count == 1024:
-				estimate_tf(m)
+				estimate_tf(m,h1)
 				sample_count = 0
 				m = Measurement()
 			sample = get_data(fifo)
